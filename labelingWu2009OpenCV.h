428f7f9a44b18f525edfa95f76dfffa144c3442b
Bolelli Federico <federico.bolelli@unimore.it>
2016-09-12 17:53:40 +0200

    moved OpenCV implementation of WU algorithm to labelingWu2009OpenCV.cpp and labelingWu2009OpenCV.h

--- labelingWu2009OpenCV.h
+++ labelingWu2009OpenCV.h
@@ -29,8 +29,4 @@
 #pragma once
 #include "opencv2/opencv.hpp"
 
-// Readable version of Grana's algorithm
-//int BBDT(const cv::Mat1b &img, cv::Mat1i &imgLabels);
-
-// Optimized version of Grana's algorithm
-int BBDT_OPT(const cv::Mat1b &img, cv::Mat1i &imgLabels);
+int SAUFCV_OPT(const cv::Mat1b &img, cv::Mat1i &labels);
