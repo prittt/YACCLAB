f3b4a039bf96570a26c47612bca2447de1ac5ea7
Michele <cancilla.michele@gmail.com>
2017-07-07 10:48:25 +0200

    Add register.h for algorithm creation/registration into the map

---
+++ include/register.h
@@ -0,0 +1,25 @@
+#ifndef YACCLAB_REGISTER_H_
+#define YACCLAB_REGISTER_H_
+
+#define REGISTER_LABELING(algorithm)                                           \
+class register_##algorithm {                                                   \
+  public:                                                                      \
+    register_##algorithm() {                                                   \
+        LabelingMapSingleton::GetInstance().data_[#algorithm] = new algorithm; \
+    }                                                                          \
+} reg_##algorithm;
+
+//#define REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(algorithm) is defined in xxx.h
+
+#define STRINGIFY(x) #x
+#define CONCAT(x,y) STRINGIFY(x ## _ ## y)
+
+#define REGISTER_SOLVER(algorithm, solver)                                                              \
+class register_ ## algorithm ## _ ## solver{                                                            \
+  public:                                                                                               \
+      register_ ## algorithm ## _ ## solver() {                                                         \
+          LabelingMapSingleton::GetInstance().data_[CONCAT(algorithm, solver)] = new algorithm<solver>; \
+  }                                                                                                     \
+}  register_ ## algorithm ## _ ## solver;
+
+#endif // !YACCLAB_REGISTER_H_
