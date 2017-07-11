9f80f529bcfd4e6e6878882ef79e88b07d9da99b
Federico Bolelli <lk_number1@hotmail.it>
2017-07-11 18:16:36 +0200

    Add base stuffs for latex report generation

--- include/latex_generator.h
+++ include/latex_generator.h
@@ -1,16 +1,24 @@
 #ifndef YACCLAB_LATEX_GENERATOR_H_
 #define YACCLAB_LATEX_GENERATOR_H_
 
+#include <map>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 
-void GenerateLatexTable(const std::string& output_path, const std::string& latex_file, const cv::Mat1d& all_res,
-    const std::vector<cv::String>& alg_name, const std::vector<cv::String>& ccl_algorithms);
+#include "file_manager.h"
 
-void GenerateMemoryLatexTable(const std::string& output_path, const std::string& latex_file, const cv::Mat1d& accesses,
-    const std::string& dataset, const std::vector<cv::String>& ccl_mem_algorithms);
+void GenerateLatexTable(const filesystem::path& output_path, const std::string& latex_file, const cv::Mat1d& all_res,
+	const std::vector<cv::String>& alg_name, const std::vector<cv::String>& ccl_algorithms);
 
-void GenerateLatexCharts(const std::string& output_path, const std::string& latex_charts, const std::string& latex_folder,
-    const std::vector<cv::String>& datasets_name);
+void GenerateMemoryLatexTable(const filesystem::path& output_path, const std::string& latex_file, const cv::Mat1d& accesses,
+	const std::string& dataset, const std::vector<cv::String>& ccl_mem_algorithms);
+
+void GenerateLatexCharts(const filesystem::path& output_path, const std::string& latex_charts, const std::string& latex_folder,
+	const std::vector<cv::String>& datasets_name);
+
+void LatexGenerator(const filesystem::path& output_path, const std::string& latex_file, const cv::Mat1d& all_res,
+	const std::vector<cv::String>& datasets_name, const std::vector<cv::String>& ccl_algorithms,
+	const std::vector<cv::String>& ccl_mem_algorithms, const std::map<std::string, cv::Mat1d>& accesses);
+
 
 #endif // !YACCLAB_LATEX_GENERATOR_H_
--- src/latex_generator.cc
+++ src/latex_generator.cc
@@ -3,176 +3,332 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <map>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 
 #include "utilities.h"
 #include "system_info.h"
+#include "file_manager.h"
 
 using namespace cv;
 using namespace std;
+using namespace filesystem;
 
 // To generate latex table with average results
-void GenerateLatexTable(const string& output_path, const string& latex_file, const Mat1d& all_res, const vector<String>& datasets_name, const vector<String>& ccl_algorithms)
+void GenerateLatexTable(const path& output_path, const string& latex_file, const Mat1d& all_res, const vector<String>& datasets_name, const vector<String>& ccl_algorithms)
 {
-    string latex_path = output_path + kPathSeparator + latex_file;
-    ofstream is(latex_path);
-    if (!is.is_open()) {
-        cout << "Unable to open/create " + latex_path << endl;
-        return;
-    }
+	//string latex_path = output_path + kPathSeparator + latex_file;
+	path latex_path = output_path / path(latex_file);
+	ofstream os(latex_path.string());
+	if (!os.is_open()) {
+		cout << "Unable to open/create " + latex_path.string() << endl;
+		return;
+	}
 
-    // fixed number of decimal values
-    is << fixed;
-    is << setprecision(3);
+	// fixed number of decimal values
+	os << fixed;
+	os << setprecision(3);
 
-    is << "%This table format needs the package 'siunitx', please uncomment and add the following line code in latex preamble if you want to add the table in your latex file" << endl;
-    is << "%\\usepackage{siunitx}" << endl << endl;
-    is << "\\begin{table}[tbh]" << endl << endl;
-    is << "\t\\centering" << endl;
-    is << "\t\\caption{Average Results in ms (Lower is Better)}" << endl;
-    is << "\t\\label{tab:table1}" << endl;
-    is << "\t\\begin{tabular}{|l|";
-    for (unsigned i = 0; i < ccl_algorithms.size(); ++i)
-        is << "S[table-format=2.3]|";
-    is << "}" << endl;
-    is << "\t\\hline" << endl;
-    is << "\t";
-    for (unsigned i = 0; i < ccl_algorithms.size(); ++i) {
-        //EraseDoubleEscape(datasets_name);
-        //datasets_name.erase(std::remove(datasets_name.begin(), datasets_name.end(), '\\'), datasets_name.end());
-        is << " & {" << ccl_algorithms[i] << "}"; //Header
-    }
-    is << "\\\\" << endl;
-    is << "\t\\hline" << endl;
+	os << "%This table format needs the package 'siunitx', please uncomment and add the following line code in latex preamble if you want to add the table in your latex file" << endl;
+	os << "%\\usepackage{siunitx}" << endl << endl;
+	os << "\\begin{table}[tbh]" << endl << endl;
+	os << "\t\\centering" << endl;
+	os << "\t\\caption{Average Results in ms (Lower os Better)}" << endl;
+	os << "\t\\label{tab:table1}" << endl;
+	os << "\t\\begin{tabular}{|l|";
+	for (unsigned i = 0; i < ccl_algorithms.size(); ++i)
+		os << "S[table-format=2.3]|";
+	os << "}" << endl;
+	os << "\t\\hline" << endl;
+	os << "\t";
+	for (unsigned i = 0; i < ccl_algorithms.size(); ++i) {
+		//RemoveCharacter(datasets_name, '\\');
+		//datasets_name.erase(std::remove(datasets_name.begin(), datasets_name.end(), '\\'), datasets_name.end());
+		os << " & {" << ccl_algorithms[i] << "}"; //Header
+	}
+	os << "\\\\" << endl;
+	os << "\t\\hline" << endl;
 
-    for (unsigned i = 0; i < datasets_name.size(); ++i) {
-        is << "\t" << datasets_name[i];
-        for (int j = 0; j < all_res.cols; ++j) {
-            is << " & ";
-            if (all_res(i, j) != numeric_limits<double>::max())
-                is << all_res(i, j); //Data
-        }
-        is << "\\\\" << endl;
-    }
-    is << "\t\\hline" << endl;
-    is << "\t\\end{tabular}" << endl << endl;
-    is << "\\end{table}" << endl;
+	for (unsigned i = 0; i < datasets_name.size(); ++i) {
+		os << "\t" << datasets_name[i];
+		for (int j = 0; j < all_res.cols; ++j) {
+			os << " & ";
+			if (all_res(i, j) != numeric_limits<double>::max())
+				os << all_res(i, j); //Data
+		}
+		os << "\\\\" << endl;
+	}
+	os << "\t\\hline" << endl;
+	os << "\t\\end{tabular}" << endl << endl;
+	os << "\\end{table}" << endl;
 
-    is.close();
+	os.close();
 }
 
 // To generate latex table with memory average accesses
-void GenerateMemoryLatexTable(const string& output_path, const string& latex_file, const Mat1d& accesses, const string& dataset, const vector<String>& ccl_mem_algorithms)
+void GenerateMemoryLatexTable(const path& output_path, const string& latex_file, const Mat1d& accesses, const string& dataset, const vector<String>& ccl_mem_algorithms)
 {
-    // TODO handle if folder does not exists
-    string latex_path = output_path + kPathSeparator + dataset + kPathSeparator + latex_file;
-    ofstream is(latex_path);
-    if (!is.is_open()) {
-        cout << "Unable to open/create " + latex_path << endl;
-        return;
-    }
+	// TODO handle if folder does not exists
+	//string latex_path = output_path + kPathSeparator + dataset + kPathSeparator + latex_file;
+	path latex_path = output_path / path(dataset) / path(latex_file);
+	ofstream os(latex_path.string());
+	if (!os.is_open()) {
+		cout << "Unable to open/create " + latex_path.string() << endl;
+		return;
+	}
 
-    // fixed number of decimal values
-    is << fixed;
-    is << setprecision(3);
+	// fixed number of decimal values
+	os << fixed;
+	os << setprecision(3);
 
-    is << "%This table format needs the package 'siunitx', please uncomment and add the following line code in latex preamble if you want to add the table in your latex file" << endl;
-    is << "%\\usepackage{siunitx}" << endl << endl;
-    is << "\\begin{table}[tbh]" << endl << endl;
-    is << "\t\\centering" << endl;
-    is << "\t\\caption{Analysis of memory accesses required by connected components computation for '" << dataset << "' dataset. The numbers are given in millions of accesses}" << endl;
-    is << "\t\\label{tab:table1}" << endl;
-    is << "\t\\begin{tabular}{|l|";
-    for (int i = 0; i < accesses.cols + 1; ++i)
-        is << "S[table-format=2.3]|";
-    is << "}" << endl;
-    is << "\t\\hline" << endl;
-    is << "\t";
+	os << "%This table format needs the package 'siunitx', please uncomment and add the following line code in latex preamble if you want to add the table in your latex file" << endl;
+	os << "%\\usepackage{siunitx}" << endl << endl;
+	os << "\\begin{table}[tbh]" << endl << endl;
+	os << "\t\\centering" << endl;
+	os << "\t\\caption{Analysis of memory accesses required by connected components computation for '" << dataset << "' dataset. The numbers are given in millions of accesses}" << endl;
+	os << "\t\\label{tab:table1}" << endl;
+	os << "\t\\begin{tabular}{|l|";
+	for (int i = 0; i < accesses.cols + 1; ++i)
+		os << "S[table-format=2.3]|";
+	os << "}" << endl;
+	os << "\t\\hline" << endl;
+	os << "\t";
 
-    // Header
-    is << "{Algorithm} & {Binary Image} & {Label Image} & {Equivalence Vector/s}  & {Other} & {Total Accesses}";
-    is << "\\\\" << endl;
-    is << "\t\\hline" << endl;
+	// Header
+	os << "{Algorithm} & {Binary Image} & {Label Image} & {Equivalence Vector/s}  & {Other} & {Total Accesses}";
+	os << "\\\\" << endl;
+	os << "\t\\hline" << endl;
 
-    for (unsigned i = 0; i < ccl_mem_algorithms.size(); ++i) {
-        // For every algorithm
-        const String& alg_name = ccl_mem_algorithms[i];
-        //EraseDoubleEscape(alg_name);
-        is << "\t{" << alg_name << "}";
+	for (unsigned i = 0; i < ccl_mem_algorithms.size(); ++i) {
+		// For every algorithm
+		const String& alg_name = ccl_mem_algorithms[i];
+		//RemoveCharacter(alg_name, '\\');
+		os << "\t{" << alg_name << "}";
 
-        double tot = 0;
+		double tot = 0;
 
-        for (int s = 0; s < accesses.cols; ++s) {
-            // For every data_ structure
-            if (accesses(i, s) != 0)
-                is << "\t& " << (accesses(i, s) / 1000000);
-            else
-                is << "\t& ";
+		for (int s = 0; s < accesses.cols; ++s) {
+			// For every data_ structure
+			if (accesses(i, s) != 0)
+				os << "\t& " << (accesses(i, s) / 1000000);
+			else
+				os << "\t& ";
 
-            tot += (accesses(i, s) / 1000000);
-        }
-        // Total Accesses
-        is << "\t& " << tot;
+			tot += (accesses(i, s) / 1000000);
+		}
+		// Total Accesses
+		os << "\t& " << tot;
 
-        // EndLine
-        is << "\t\\\\" << endl;
-    }
+		// EndLine
+		os << "\t\\\\" << endl;
+	}
 
-    // EndTable
-    is << "\t\\hline" << endl;
-    is << "\t\\end{tabular}" << endl << endl;
-    is << "\\end{table}" << endl;
+	// EndTable
+	os << "\t\\hline" << endl;
+	os << "\t\\end{tabular}" << endl << endl;
+	os << "\\end{table}" << endl;
 
-    is.close();
+	os.close();
 }
 
-void GenerateLatexCharts(const string& output_path, const string& latex_charts, const string& latex_folder, const vector<String>& datasets_name)
+void GenerateLatexCharts(const filesystem::path& output_path, const string& latex_charts, const string& latex_folder, const vector<String>& datasets_name)
 {
-    string latex_path = output_path + kPathSeparator + latex_folder + kPathSeparator + latex_charts;
-    ofstream is(latex_path);
-    if (!is.is_open()) {
-        cout << "Unable to open/create " + latex_path << endl;
-        return;
-    }
+	//string latex_path = output_path + kPathSeparator + latex_folder + kPathSeparator + latex_charts;
+	path latex_path = output_path / path(latex_folder) / path(latex_charts);
+	ofstream os(latex_path.string());
+	if (!os.is_open()) {
+		cout << "Unable to open/create " + latex_path.string() << endl;
+		return;
+	}
 
-    SystemInfo info;
-    string chartSize{ "0.45" }, chartWidth{ "1" };
+	SystemInfo info;
+	string chartSize{ "0.45" }, chartWidth{ "1" };
 
-    // Get information about date and time
-    string datetime = GetDatetime();
+	// Get information about date and time
+	string datetime = GetDatetime();
 
-    // fixed number of decimal values
-    is << fixed;
-    is << setprecision(3);
+	// fixed number of decimal values
+	os << fixed;
+	os << setprecision(3);
 
-    is << "%These charts are generated using YACCLAB. Follow our project on GitHub: https://github.com/prittt/YACCLAB" << endl << endl;
-    is << "\\begin{figure}[b]" << endl << endl;
-    //\newcommand{ \machineName }{x86\_MSVC2015\_Windows10}
-    is << "\t\\newcommand{\\machineName}{";
-    is << info << "}" << endl;
+	os << "%These charts are generated using YACCLAB. Follow our project on GitHub: https://github.com/prittt/YACCLAB" << endl << endl;
+	os << "\\begin{figure}[b]" << endl << endl;
+	//\newcommand{ \machineName }{x86\_MSVC2015\_Windows10}
+	os << "\t\\newcommand{\\machineName}{";
+	os << info << "}" << endl;
 
-    //replace the . with _ for filenames
-    pair<string, string> compiler(SystemInfo::GetCompiler());
-    replace(compiler.first.begin(), compiler.first.end(), '.', '_');
-    replace(compiler.second.begin(), compiler.second.end(), '.', '_');
+	//replace the . with _ for filenames
+	pair<string, string> compiler(SystemInfo::GetCompiler());
+	std::replace(compiler.first.begin(), compiler.first.end(), '.', '_');
+	std::replace(compiler.second.begin(), compiler.second.end(), '.', '_');
 
-    //\newcommand{ \compilerName }{MSVC2015}
-    is << "\t\\newcommand{\\compilerName}{" + compiler.first + compiler.second + "}" << endl;
+	//\newcommand{ \compilerName }{MSVC2015}
+	os << "\t\\newcommand{\\compilerName}{" + compiler.first + compiler.second + "}" << endl;
 
-    is << "\t\\centering" << endl;
+	os << "\t\\centering" << endl;
 
-    for (unsigned i = 0; i < datasets_name.size(); ++i) {
-        is << "\t\\begin{subfigure}[b]{" + chartSize + "\\textwidth}" << endl;
-        is << "\t\t\\caption{" << datasets_name[i] + "}" << endl;
-        is << "\t\t\\centering" << endl;
-        is << "\t\t\\includegraphics[width=" + chartWidth + "\\textwidth]{\\compilerName " + datasets_name[i] + ".pdf}" << endl;
-        is << "\t\\end{subfigure}" << endl << endl;
-    }
+	for (unsigned i = 0; i < datasets_name.size(); ++i) {
+		os << "\t\\begin{subfigure}[b]{" + chartSize + "\\textwidth}" << endl;
+		os << "\t\t\\caption{" << datasets_name[i] + "}" << endl;
+		os << "\t\t\\centering" << endl;
+		os << "\t\t\\includegraphics[width=" + chartWidth + "\\textwidth]{\\compilerName " + datasets_name[i] + ".pdf}" << endl;
+		os << "\t\\end{subfigure}" << endl << endl;
+	}
 
-    is << "\t\\caption{\\machineName}\t" + datetime << endl << endl;
-    is << "\\end{figure}" << endl << endl;
+	os << "\t\\caption{\\machineName}\t" + datetime << endl << endl;
+	os << "\\end{figure}" << endl << endl;
 
-    is.close();
+	os.close();
+}
+
+void LatexGenerator(const path& output_path, const string& latex_file, const Mat1d& all_res, const vector<String>& datasets_name, const vector<String>& ccl_algorithms, const vector<String>& ccl_mem_algorithms, const map<string, Mat1d>& accesses)
+{
+	//string latex_path = output_path + kPathSeparator + latex_file;
+	path latex_path = output_path / path(latex_file);
+	ofstream os(latex_path.string());
+	if (!os.is_open()) {
+		cout << "Unable to open/create " + latex_path.string() << endl;
+		return;
+	}
+
+	// fixed number of decimal values
+	os << fixed;
+	os << setprecision(3);
+
+	// Document begin
+	os << "\\documentclass{article}" << endl;
+	os << "%This table format needs the package 'siunitx', please uncomment and add the following line code in latex preamble if you want to add the table in your latex file" << endl;
+	os << "%\\usepackage{siunitx}" << endl << endl;
+	os << "\\begin{document}" << endl << endl;
+
+	// Section average results table ------------------------------------------------------------------------------------------
+	os << "\\section{Average Results}" << endl << endl;
+
+	os << "\\begin{table}[tbh]" << endl << endl;
+	os << "\t\\centering" << endl;
+	os << "\t\\caption{Average Results in ms (Lower os Better)}" << endl;
+	os << "\t\\label{tab:table1}" << endl;
+	os << "\t\\begin{tabular}{|l|";
+	for (unsigned i = 0; i < ccl_algorithms.size(); ++i)
+		os << "S[table-format=2.3]|";
+	os << "}" << endl;
+	os << "\t\\hline" << endl;
+	os << "\t";
+	for (unsigned i = 0; i < ccl_algorithms.size(); ++i) {
+		//RemoveCharacter(datasets_name, '\\');
+		//datasets_name.erase(std::remove(datasets_name.begin(), datasets_name.end(), '\\'), datasets_name.end());
+		os << " & {" << ccl_algorithms[i] << "}"; //Header
+	}
+	os << "\\\\" << endl;
+	os << "\t\\hline" << endl;
+	for (unsigned i = 0; i < datasets_name.size(); ++i) {
+		os << "\t" << datasets_name[i];
+		for (int j = 0; j < all_res.cols; ++j) {
+			os << " & ";
+			if (all_res(i, j) != numeric_limits<double>::max())
+				os << all_res(i, j); //Data
+		}
+		os << "\\\\" << endl;
+	}
+	os << "\t\\hline" << endl;
+	os << "\t\\end{tabular}" << endl << endl;
+	os << "\\end{table}" << endl;
+
+	// Section figures with charts ---------------------------------------------------------------------------
+	SystemInfo info;
+	string chartSize{ "0.45" }, chartWidth{ "1" };
+	// Get information about date and time
+	string datetime = GetDatetime();
+
+	os << "\\section{Average and Density Charts}" << endl << endl;
+
+	os << "%These charts are generated using YACCLAB. Follow our project on GitHub: https://github.com/prittt/YACCLAB" << endl << endl;
+	os << "\\begin{figure}[b]" << endl << endl;
+	//\newcommand{ \machineName }{x86\_MSVC2015\_Windows10}
+	os << "\t\\newcommand{\\machineName}{";
+	os << info << "}" << endl;
+
+	//replace the . with _ for filenames
+	pair<string, string> compiler(SystemInfo::GetCompiler());
+	std::replace(compiler.first.begin(), compiler.first.end(), '.', '_');
+	std::replace(compiler.second.begin(), compiler.second.end(), '.', '_');
+
+	//\newcommand{ \compilerName }{MSVC2015}
+	os << "\t\\newcommand{\\compilerName}{" + compiler.first + compiler.second + "}" << endl;
+
+	os << "\t\\centering" << endl;
+
+	for (unsigned i = 0; i < datasets_name.size(); ++i) {
+		os << "\t\\begin{subfigure}[b]{" + chartSize + "\\textwidth}" << endl;
+		os << "\t\t\\caption{" << datasets_name[i] + "}" << endl;
+		os << "\t\t\\centering" << endl;
+		os << "\t\t\\includegraphics[width=" + chartWidth + "\\textwidth]{\\compilerName " + datasets_name[i] + ".pdf}" << endl;
+		os << "\t\\end{subfigure}" << endl << endl;
+	}
+
+	os << "\t\\caption{\\machineName}\t" + datetime << endl << endl;
+	os << "\\end{figure}" << endl << endl;
+
+
+	// Section memory result table ---------------------------------------------------------------------------
+	if (ccl_mem_algorithms.size() > 0) {
+		os << "\\section{Memory tests}" << endl << endl;
+
+		for (const auto& dataset : accesses) {
+
+			const auto& dataset_name = dataset.first;
+			const auto& accesses = dataset.second;
+
+			os << "\\begin{table}[tbh]" << endl << endl;
+			os << "\t\\centering" << endl;
+			os << "\t\\caption{Analysis of memory accesses required by connected components computation for '" << dataset_name << "' dataset. The numbers are given in millions of accesses}" << endl;
+			os << "\t\\label{tab:table1}" << endl;
+			os << "\t\\begin{tabular}{|l|";
+			for (int i = 0; i < accesses.cols + 1; ++i)
+				os << "S[table-format=2.3]|";
+			os << "}" << endl;
+			os << "\t\\hline" << endl;
+			os << "\t";
+
+			// Header
+			os << "{Algorithm} & {Binary Image} & {Label Image} & {Equivalence Vector/s}  & {Other} & {Total Accesses}";
+			os << "\\\\" << endl;
+			os << "\t\\hline" << endl;
+
+			for (unsigned i = 0; i < ccl_mem_algorithms.size(); ++i) {
+				// For every algorithm
+				const String& alg_name = ccl_mem_algorithms[i];
+				//RemoveCharacter(alg_name, '\\');
+				os << "\t{" << alg_name << "}";
+
+				double tot = 0;
+
+				for (int s = 0; s < accesses.cols; ++s) {
+					// For every data_ structure
+					if (accesses(i, s) != 0)
+						os << "\t& " << (accesses(i, s) / 1000000);
+					else
+						os << "\t& ";
+
+					tot += (accesses(i, s) / 1000000);
+				}
+				// Total Accesses
+				os << "\t& " << tot;
+
+				// EndLine
+				os << "\t\\\\" << endl;
+			}
+
+			// EndTable
+			os << "\t\\hline" << endl;
+			os << "\t\\end{tabular}" << endl << endl;
+			os << "\\end{table}" << endl;
+		}
+	}
+
+
+	os << "\\end{document}";
+	os.close();
 }
