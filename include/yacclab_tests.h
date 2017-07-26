d1b8fd996437cb799832cf5c667e09969d495197
Michele <cancilla.michele@gmail.com>
2017-07-26 10:31:25 +0200

    Add new public containers which will store memory, average and average with steps results

--- include/yacclab_tests.h
+++ include/yacclab_tests.h
@@ -4,33 +4,46 @@
 
 #include <map>
 
+#include <opencv2/imgproc.hpp>
+
 #include "config_data.h"
 #include "file_manager.h"
+#include "labeling_algorithms.h"
 
 class YacclabTests {
 public:
-	YacclabTests(const ConfigData& cfg) : cfg_(cfg) {}
+    YacclabTests(const ConfigData& cfg) : cfg_(cfg)
+    {
+        average_results_ = cv::Mat1d(cfg_.average_datasets.size(), cfg_.ccl_algorithms.size(), std::numeric_limits<double>::max());
+        average_ws_results_ = cv::Mat1d(cfg_.average_datasets.size(), cfg_.ccl_algorithms.size() * StepType::ST_SIZE, std::numeric_limits<double>::max());
+    }
 
-	void CheckPerformLabeling();
-	void CheckPerformLabelingWithSteps();
-	void CheckPerformLabelingMem();
+    void CheckPerformLabeling();
+    void CheckPerformLabelingWithSteps();
+    void CheckPerformLabelingMem();
 
-	void CheckAlgorithms(); // TODO move private
+    void CheckAlgorithms(); // TODO move private
 
-    void AverageTest();
-    void AverageTestWithSteps();
+    void AverageTest(cv::Mat1d& average_results_);
+    void AverageTestWithSteps(cv::Mat1d& average_ws_results_);
 
-	//TODO: Check correctness of memory tests also
+    //TODO: Check correctness of memory tests also
 
-	//Other test functions
+    //Other test functions
 
 private:
-	ConfigData cfg_;
-	
-	bool LoadFileList(std::vector<std::pair<std::string, bool>>& filenames, const filesystem::path& files_path);
-	
+    ConfigData cfg_;
+    cv::Mat1d average_results_;
+    cv::Mat1d average_ws_results_;
+    std::map<std::string, cv::Mat1d> memory_accesses_;
+
+
+    bool LoadFileList(std::vector<std::pair<std::string, bool>>& filenames, const filesystem::path& files_path);
+
     void SaveBroadOutputResults(std::map<cv::String, cv::Mat1d>& results, const std::string& o_filename, const cv::Mat1i& labels, const std::vector<std::pair<std::string, bool>>& filenames);
+    void SaveBroadOutputResults(const cv::Mat1d& results, const std::string& o_filename, const cv::Mat1i& labels, const std::vector<std::pair<std::string, bool>>& filenames);
+
+    void LatexGenerator();
 };
 
 #endif // !YACCLAB_YACCLAB_TESTS_H_
-
