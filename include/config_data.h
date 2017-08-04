0b7df98f09941e999f6a3ae9c56184a892dd1986
Federico Bolelli <lk_number1@hotmail.it>
2017-08-04 14:51:17 +0200

    Remove unused variable

--- include/config_data.h
+++ include/config_data.h
@@ -73,7 +73,6 @@ struct ConfigData {
     path output_path;                     // Path on which results are stored
     path input_path;                      // Path on which input datasets are stored
     path latex_path;                      // Path on which latex report will be stored
-    path logfile_path;                    // Path on output log file
 
     std::vector<cv::String> check_datasets;       // List of datasets on which check tests will be performed
     std::vector<cv::String> memory_datasets;      // List of datasets on which memory tests will be perform
@@ -127,7 +126,6 @@ struct ConfigData {
         output_path                  = path(fs["paths"]["output"]) / path(GetDatetimeWithoutSpecialChars());
         input_path                   = path(fs["paths"]["input"]);
         latex_path                   = output_path / path("latex");
-        logfile_path                 = output_path / path("log.txt");
 
         check_datasets               = std::vector<cv::String>(fs["check_datasets"].size());
         cv::read(fs["check_datasets"], check_datasets);
