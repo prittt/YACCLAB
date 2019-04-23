6067cd64a5fbfe9f208658ecd16492c6110883f8
Federico Bolelli <lk_number1@hotmail.it>
2019-04-23 12:40:41 +0200

    Force travis-ci to use CMake 3.13

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright(c) 2016 - 2017 Federico Bolelli
+# Copyright(c) 2016 - 2019 Federico Bolelli
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -28,37 +28,6 @@
 # OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# List of OpenCV 3.1.0 modules
-#-D BUIL_opencv_core=ON
-#-D BUIL_opencv_imgproc=ON
-#-D BUIL_opencv_imgcodecs=ON
-#-D BUIL_opencv_videoio=OFF 
-#-D BUIL_opencv_highgui=OFF 
-#-D BUIL_opencv_video=OFF
-#-D BUIL_opencv_calib3d=OFF
-#-D BUIL_opencv_features2d=OFF
-#-D BUIL_opencv_objdetect=OFF
-#-D BUIL_opencv_ml=OFF
-#-D BUIL_opencv_flann=OFF
-#-D BUIL_opencv_photo=OFF
-#-D BUIL_opencv_stitching=OFF
-#-D BUIL_opencv_cudaarithm=OFF
-#-D BUIL_opencv_cudabgsegm=OFF
-#-D BUIL_opencv_cudacodec=OFF
-#-D BUIL_opencv_cudafeatures2d=OFF
-#-D BUIL_opencv_cudafilters=OFF
-#-D BUIL_opencv_cudaimgproc=OFF
-#-D BUIL_opencv_cudalegacy=OFF
-#-D BUIL_opencv_cudaobjdetect=OFF
-#-D BUIL_opencv_cudaoptflow=OFF
-#-D BUIL_opencv_cudastereo=OFF
-#-D BUIL_opencv_cudawarping=OFF
-#-D BUIL_opencv_cudev=OFF
-#-D BUIL_opencv_shape=OFF
-#-D BUIL_opencv_superres=OFF
-#-D BUIL_opencv_videostab=OFF
-#-D BUIL_opencv_viz=OFF
-
 # Exit this script if any commmand fails 
 set -e
 
@@ -84,7 +53,22 @@ function install_linux_environment()
   #sudo apt-get install build-essential gcc-multilib gcc-4.8-multilib g++-multilib g++-4.8-multilib lib32z1 lib32ncurses5 lib32bz2-1.0 libc6-dev libgmp-dev libmpfr-dev libmpc-dev
   #sudo dpkg --add-architecture i386
   #sudo ln -s /usr/include/x86_64-linux-gnu/zconf.h /usr/include
-  #echo -e "------------------------------------------> DONE!"  
+  #echo -e "------------------------------------------> DONE!"
+  
+  echo -e "\n\n------------------------------------------> Install cmake-3.13:"
+  export DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+  mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+  curl -L https://cmake.org/files/v3.13/cmake-3.13.0-Linux-x86_64.tar.gz > cmake-3.13.tar.gz
+  echo -e "    DONE"
+  tar -xzvf cmake-3.13.tar.gz
+  mv cmake-3.13.0-Linux-x86_64 cmake-install
+  export PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
+  cd ${TRAVIS_BUILD_DIR}
+  echo -e "------------------------------------------> DONE!" 
+
+  echo -e "\n\n------------------------------------------> Check CMake version"
+  cmake --version
+  echo -e "------------------------------------------> DONE!"   
   
   echo -e "\n\n------------------------------------------> Install OpenCV-3.1.0 (only if they weren't cached) and dependent packages:"
   sudo apt-get -qq -y install build-essential
@@ -168,6 +152,17 @@ function install_osx_environment()
   #brew upgrade -y > /dev/null
   #brew update -y > brew_update.log
   #brew upgrade -y > brew_upgrade.log
+  echo -e "------------------------------------------> DONE!" 
+  
+  echo -e "\n\n------------------------------------------> Install cmake-3.13:"
+  export DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+  mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+  curl -L https://cmake.org/files/v3.13/cmake-3.13.0-Darwin-x86_64.tar.gz > cmake-3.13.tar.gz
+  echo -e "    DONE"
+  tar -xzvf cmake-3.13.tar.gz
+  mv cmake-3.13.0-Darwin-x86_64 cmake-install
+  export PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
+  cd ${TRAVIS_BUILD_DIR}
   echo -e "------------------------------------------> DONE!" 
   
   echo -e "\n\n------------------------------------------> Check CMake version"
