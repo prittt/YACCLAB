aea5f032e287189d8083c9d17d0df55c2e426da5
stal12 <stefano.allegretti@unimore.it>
2021-06-01 12:12:47 +0200

    Add labeling_bmrs and labeling_brts

---
+++ src/labeling_bmrs.cc
@@ -0,0 +1,4 @@
+#include "labeling_bmrs.h"
+#include "labels_solver.h"
+
+REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(BMRS);
---
+++ src/labeling_brts.cc
@@ -0,0 +1,4 @@
+#include "labeling_brts.h"
+#include "labels_solver.h"
+
+REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(BRTS);
